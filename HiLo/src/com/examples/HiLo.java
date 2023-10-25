038bdc9d0a071a5b944a700254b74774f2123e1d
Karolina Jonczyk <253737@student.pwr.edu.pl>
2023-10-25 15:02:04 +0200

    New options in HiLo game

--- HiLo/src/com/examples/HiLo.java
+++ HiLo/src/com/examples/HiLo.java
@@ -6,18 +6,20 @@ public class HiLo {
 		Scanner scan = new Scanner(System.in);
 		String playAgain = "";
 		do {
-			int myNum = (int)(Math.random() * 100 + 1);
+			int myNum = (int)(Math.random() * 201 - 100);
 			//System.out.println( myNum );
 			int guess = 0;
+			int tries = 0;
 			while (guess != myNum) {
-				System.out.println("Guess a number between 1 and 100:");
+				System.out.println("Guess a number between -100 and 100:");
 				guess = scan.nextInt();
+				tries = tries + 1;
 				if (guess < myNum)
 					System.out.println(guess + " is too low. Try again.");
 				else if (guess > myNum)
 					System.out.println(guess + " is too high. Try again.");
 				else
-					System.out.println("You entered " + guess + ". You win!");
+					System.out.println("You entered " + guess + ". You win!\nNumber of tries: " + tries + ". Congratulations!");
 			}
 			System.out.println("Would you like to play again? (y/n)");
 			playAgain = scan.next();
